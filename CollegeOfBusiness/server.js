5e3f1f94ed4aaa8e67b7a43c8478b4d5b56f1bbe
Jonathan Chambers <jonathan.chambers@selu.edu>
2018-09-12 22:51:48 -0500

    we have a working database

--- CollegeOfBusiness/server.js
+++ CollegeOfBusiness/server.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var chalk = require("chalk")
-//var mongodb = require("mongodb")
+var mongodb = require("mongodb")
 //var ObjectID = mongodb.ObjectID
 
 /*
@@ -17,11 +17,26 @@ var distDir = __dirname + "/dist/CollegeOfBusiness"
 app.use(express.static(distDir))
 console.log(__dirname)
 
-var server = app.listen(8080, function () {
-    var port = server.address().port
-    console.log(chalk.blue( "App now running on port", port))
-})
+var db
+var testCollection = "tests"
 
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect("mongodb://411admin:411password@ds149672.mlab.com:49672/411db", function (err, client) {
+    if (err) {
+        console.log(err)
+        process.exit(1)
+    }
+
+    // Save database object from the callback for reuse.
+    db = client.db("411db")
+    console.log(chalk.green("Database connection ready"))
+
+
+    var server = app.listen(8080, function () {
+        var port = server.address().port
+        console.log(chalk.blue( "App now running on port", port))
+    })
+})
 
 /*
 *establing api routes
@@ -32,5 +47,21 @@ function handleError(res, reason, message, code) {
 }
 
 router.get('/test', function(req,res) {
-    res.status(200).json({string: "This is just to demonstrate using the api to retrieve json."})
+    db.collection(testCollection).find({}).toArray(function(err, docs) {
+        if (err) {
+          handleError(res, err.message, "Failed to get tests.")
+        } else {
+          res.status(200).json(docs)
+        }
+      })
+})
+router.post('/test', function(req,res) {
+    var newTest = req.body
+    db.collection(testCollection).insertOne(newTest, function(err, doc){
+        if (err){
+            console.log(err)
+        } else{
+            res.status(201).json(doc.ops[0])
+        }
+    })
 })
